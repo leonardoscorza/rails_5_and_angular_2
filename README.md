8972d3f1e465a8a431b7c8d8a39a350c62d8092d
Leonardo Scorza <leonardoscorzas88@gmail.com>
2016-12-10 03:17:22 -0200

    first commit

---
+++ README.md
@@ -0,0 +1,4 @@
+* Conectando Angular 2 com Rails 5
+
+App desenvolvido como um tutorial para o blog http://onebitcode.com
+
---
+++ test/controllers/questions_controller_test.rb
@@ -0,0 +1,38 @@
+require 'test_helper'
+
+class QuestionsControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @question = questions(:one)
+  end
+
+  test "should get index" do
+    get questions_url, as: :json
+    assert_response :success
+  end
+
+  test "should create question" do
+    assert_difference('Question.count') do
+      post questions_url, params: { question: { text: @question.text, title: @question.title } }, as: :json
+    end
+
+    assert_response 201
+  end
+
+  test "should show question" do
+    get question_url(@question), as: :json
+    assert_response :success
+  end
+
+  test "should update question" do
+    patch question_url(@question), params: { question: { text: @question.text, title: @question.title } }, as: :json
+    assert_response 200
+  end
+
+  test "should destroy question" do
+    assert_difference('Question.count', -1) do
+      delete question_url(@question), as: :json
+    end
+
+    assert_response 204
+  end
+end
